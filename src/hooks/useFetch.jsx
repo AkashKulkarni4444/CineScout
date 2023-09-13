6545085c39ef8cb3bfa5f0f5fdf324bdc0443577
Akash <akashkulkarni4444@gmail.com>
2023-09-13 22:31:52 +0530

    1) Completed Details Banner 2) Added Play Button to play trailer 3) Added video popup to show video playing 4) More sections in details to be added in future.

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -2,26 +2,25 @@ import { useEffect, useState } from "react";
 import { fetchDataFromApi } from "../utils/api";
 
 const useFetch = (url) => {
-    const [data, setData] = useState("");
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState("");
-    useEffect(() => {
-        setLoading(true);
-        setData("");
-        setError("");
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    setLoading(true);
+    setData(null);
+    setError(null);
+    fetchDataFromApi(url)
+      .then((res) => {
+        setLoading(false);
+        setData(res);
+      })
+      .catch((err) => {
+        setLoading(false);
+        setError("Something went wrong!");
+      });
+  }, [url]);
 
-        fetchDataFromApi(url)
-            .then((res) => {
-                setLoading(false);
-                setData(res);
-            })
-            .catch((err) => {
-                setLoading(false);
-                setError("Something went wrong!");
-            });
-    }, [url]);
-
-    return { data, loading, error };
+  return { data, loading, error };
 };
 
 export default useFetch;
