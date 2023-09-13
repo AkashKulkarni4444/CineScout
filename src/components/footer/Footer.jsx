a9ab783a1a4702ceebf48cb8416f944c57240888
Akash <akashkulkarni4444@gmail.com>
2023-09-14 01:31:42 +0530

    Minor Changes and customization to website.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,51 +1,62 @@
 import React from "react";
-import {
-    FaFacebookF,
-    FaInstagram,
-    FaTwitter,
-    FaLinkedin,
-} from "react-icons/fa";
+import { FaInstagram, FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
 
 import "./style.scss";
 
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <ContentWrapper>
-                <ul className="menuItems">
-                    <li className="menuItem">Terms Of Use</li>
-                    <li className="menuItem">Privacy-Policy</li>
-                    <li className="menuItem">About</li>
-                    <li className="menuItem">Blog</li>
-                    <li className="menuItem">FAQ</li>
-                </ul>
-                <div className="infoText">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
-                </div>
-                <div className="socialIcons">
-                    <span className="icon">
-                        <FaFacebookF />
-                    </span>
-                    <span className="icon">
-                        <FaInstagram />
-                    </span>
-                    <span className="icon">
-                        <FaTwitter />
-                    </span>
-                    <span className="icon">
-                        <FaLinkedin />
-                    </span>
-                </div>
-            </ContentWrapper>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">
+          🎬 Welcome to the CineScout! 🍿 🚀 Developed by Akash Kulkarni, this
+          website showcases the perfect blend of creativity and technical
+          expertise. Tech stack for this website includes React, SCSS, Redux,
+          and Redux Toolkit, ensuring a seamless and engaging user experience.
+          🌟 As movie enthusiasts myself, I have poured my efforts and passion
+          into creating this platform. Here, you'll find information about your
+          favorite films, from plot summaries to cast details and more. 🤝 I am
+          on the lookout for an internship opportunity to further enhance my
+          skills and contribute to exciting projects. If you're interested in
+          collaborating with a motivated student developer, I'd love to hear
+          from you! Feel free to reach out via the contact information provided.
+        </div>
+        <div className="infoText" >
+        EmailId : akashkulkarni4444@gmail.com        Phone Number: +91 9834947924
+        </div>
+        <div className="socialIcons">
+          <a href="https://github.com/AkashKulkarni4444">
+            <span className="icon">
+              <FaGithub />
+            </span>
+          </a>
+          <a href="https://www.instagram.com/akashkulkarni4444/">
+            <span className="icon">
+              <FaInstagram />
+            </span>
+          </a>
+          {/* <a href="akashkulkarni4444@gmail.com">
+            <span className="icon">
+              <FaEnvelope />
+            </span>
+          </a> */}
+          <a href="https://www.linkedin.com/in/akashkulkarni4444/">
+            <span className="icon">
+              <FaLinkedin />
+            </span>
+          </a>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
 };
 
 export default Footer;
